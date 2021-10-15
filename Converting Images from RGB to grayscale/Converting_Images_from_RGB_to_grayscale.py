4779426b2cb9f28d76c8b96ecdaa0154e0e27f0c
Musa17 <amusul00@fesb.hr>
2021-10-15 19:17:49 +0200

    Adding convertion from RGB to grayscale

--- Converting Images from RGB to grayscale/Converting_Images_from_RGB_to_grayscale.py
+++ Converting Images from RGB to grayscale/Converting_Images_from_RGB_to_grayscale.py
@@ -13,3 +13,14 @@ cv2.destroyAllWindows()
 rows = originalImage.shape[0]
 columns = originalImage.shape[1]
 canals = originalImage.shape[2]
+
+
+print ("Please wait a little bit ...")
+
+# Convert RGB image to grayscale image
+for k in range(0, canals):
+	for i in range(0, rows):
+		for j in range(0, columns):
+			(b, g, r) = originalImage[i, j]
+			originalImage[i, j] = (0.299 * r) + (0.587 * g) + (0.114 * b)
+
