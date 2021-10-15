b9ff00c502086914ccc31070642316785a21d4a6
Musa17 <amusul00@fesb.hr>
2021-10-15 19:13:08 +0200

    Separating R, G and B component of image

--- Converting Images from RGB to grayscale/Converting_Images_from_RGB_to_grayscale.py
+++ Converting Images from RGB to grayscale/Converting_Images_from_RGB_to_grayscale.py
@@ -9,3 +9,7 @@ cv2.imshow('Original image', originalImage)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+rows = originalImage.shape[0]
+columns = originalImage.shape[1]
+canals = originalImage.shape[2]
