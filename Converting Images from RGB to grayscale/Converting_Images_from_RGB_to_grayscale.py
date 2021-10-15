f44cdbd487f0579c2213b3e8650ddecd9bff9582
Musa17 <amusul00@fesb.hr>
2021-10-15 19:11:38 +0200

    Loading original image

--- Converting Images from RGB to grayscale/Converting_Images_from_RGB_to_grayscale.py
+++ Converting Images from RGB to grayscale/Converting_Images_from_RGB_to_grayscale.py
@@ -1,1 +1,11 @@
 import cv2
+
+# Load image
+originalImage = cv2.imread('Pikachu.jpg')
+
+# Show original image
+cv2.namedWindow('Original image')
+cv2.imshow('Original image', originalImage)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
